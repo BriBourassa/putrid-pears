cce1ce1c2cdb19873110ce17c4af8928539b103e
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-05-10 18:57:29 -0600

    Feat: build component structure, add fun font

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>(PUTRID_PEARS)</h1>
+      <Search />
+      <MovieTile />
+
+    </div>
+  );
+}
+
+export default App;
