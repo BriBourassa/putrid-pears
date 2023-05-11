ce38b7fc59ac3a93ed153e504eaa0e646f8a75c6
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-05-11 15:13:57 -0600

    set up hooks and fetch in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,28 @@
+import React, { useState, useEffect } from 'react';
+// import getMovies from '../../APIcalls';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [allMovieTiles, setAllMovieTiles] = useState([])
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+  
+  const getMovies = (path = "") => {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${path}`)
+    .then(response => response.json())
+    .then(data => setAllMovieTiles({ allMovieTiles: data.movies}))
+  }
+  
+  
+  
   return (
     <div className="App">
+      {console.log(allMovieTiles)}
       <h1>(PUTRID_PEARS)</h1>
-      <Search />
-      <MovieTile />
+      
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+    
     <App />
+  
   
 );
 
