95d656980ee27814c0052a9b1daa3d9f2162b30e
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-05-12 09:41:15 -0600

    Add flicker and pulsate keyframes to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,1 +1,18 @@
 // holds title, home button, and search bar
+import './NavBar.css';
+import pear from '../../assets/pear2.png'
+import pearshadow from '../../assets/pearshadow.png'
+
+const NavBar = () => {
+return (
+    <div className="navbar">
+        <img src={pear}/>
+        <h1>(PUTRID_PEARS)</h1>
+        <img src={pearshadow}/>
+    </div>
+)
+}
+
+
+
+export default NavBar
