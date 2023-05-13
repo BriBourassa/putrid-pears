d4792148e6d656847e7b3ad58bc7fdab4a1bfdd4
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-05-12 22:16:24 -0600

    feat: add glow hover effect to movie tiles

--- src/components/MovieTile/MovieTile.js
+++ src/components/MovieTile/MovieTile.js
@@ -5,8 +5,6 @@ const MovieTile = ({ movieDetails }) => {
   const { title, poster_path } = movieDetails
   return (
     <div className="movie-tile">
-
-      <h2 className="tile-heading">{title}</h2>
       <img className="poster" src={poster_path}/>
     </div>
   )
