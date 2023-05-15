676e554359e52d368181bbb37b2e623e9cd14272
Shane Misra <shane.misra@gmail.com>
2023-05-15 13:35:21 -0600

    further style movieDeets component, some global styles to compare

--- src/components/MovieDeets/MovieDeets.js
+++ src/components/MovieDeets/MovieDeets.js
@@ -3,7 +3,7 @@ import './MovieDeets.css'
 import {Link} from 'react-router-dom'
 
 const MovieDeets = ( {selectedMovie, getData, match} ) => {
-  // console.log('ln 6 deets', selectedMovie)
+  console.log('ln 6 deets', selectedMovie)
   console.log('match', match.params.movieID)
   // if (selectedMovie) {
   //   console.log(selectedMovie)
@@ -17,14 +17,14 @@ const MovieDeets = ( {selectedMovie, getData, match} ) => {
     }
   }, [])
 
-  const { id, backdrop_path, title, overview} = selectedMovie.selectedMovie
-
+  const { id, poster_path, title, overview, backdrop_path, tagline} = selectedMovie.selectedMovie
   return (
-    <Link to={`/movies/${id}`}>
-    <div className='movie-detail-view'>
-      <img src={backdrop_path} alt={title}/>
+    <Link to={`/movies/${id}`} className='router-link'>
       <h2 className='selected-title'>{title}</h2>
-      <p>{overview}</p>
+    <div className='movie-detail-view'>
+      <h3 className='selected-tagline'>{tagline}</h3>
+      <img src={poster_path} alt={title} className='poster'/>
+      <p className='movie-overview'>{overview}</p>
     </div>
     </Link>
   )
