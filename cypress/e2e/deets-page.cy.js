25287027e67a4260b637cbc10973e9de514608cb
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-05-18 15:27:31 -0600

    separated tests into two files, finish main page tests, write second command

---
+++ cypress/e2e/deets-page.cy.js
@@ -0,0 +1,19 @@
+describe('Details Page', () => {
+    beforeEach(() => {
+      cy.loadDetailsPage()
+      cy.visit('http://localhost:3000/')
+    });
+
+
+  // it('should be able to click a movie and see that movie's details', () => {
+    
+  // })
+
+  // it('should display an error that no movie was found when user searches for a movie not in the list', () => {
+
+  // })
+
+
+  // it('should return to main page when home button is clicked, where all movies are displayed', () => {
+
+  })
