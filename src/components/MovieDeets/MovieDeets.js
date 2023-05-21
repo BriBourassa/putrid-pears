b7533ab0db6b3bc5b51537888eb3064b4e23cf0c
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-05-21 14:44:25 -0600

    clean up code and add pears

--- src/components/MovieDeets/MovieDeets.js
+++ src/components/MovieDeets/MovieDeets.js
@@ -1,35 +1,54 @@
-import React from 'react';
-import './MovieDeets.css'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import React from "react";
+import "./MovieDeets.css";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import pearshadow from "../../assets/pearshadow.png";
 
 const MovieDeets = ({ selectedMovie }) => {
-
-  const { id, poster_path, title, overview, backdrop_path, tagline, average_rating, runtime, release_date} = selectedMovie
+  const {
+    id,
+    poster_path,
+    title,
+    overview,
+    backdrop_path,
+    tagline,
+    average_rating,
+    runtime,
+    release_date,
+  } = selectedMovie;
 
   const backdrop = {
-    'backgroundImage': `linear-gradient(rgb(0 0 0 / 17%) 0%, #000 100%), url(${backdrop_path})`
-  }
+    backgroundImage: `linear-gradient(rgb(0 0 0 / 17%) 0%, #000 100%), url(${backdrop_path})`,
+  };
 
   return (
-    <Link to={`/movies/${id}`} className='router-link' style={backdrop}>
-      <h2 className='selected-title'>{title}</h2>
-      <h3 className='selected-tagline'>{tagline}</h3>
-      <div className='movie-detail-main'>
-        <img src={poster_path} alt={title} className='poster'/>
-        <span className='detail'>Rating: {average_rating} / 10</span>
-        <span className='detail'>Runtime: {runtime} minutes</span>
-        <span className='detail'>Release Date: {release_date}</span>
-        <span className='movie-overview'>{overview}</span>
+    <Link to={`/movies/${id}`} className="router-link" style={backdrop}>
+      <h2 className="selected-title">{title}</h2>
+      <h3 className="selected-tagline">{tagline}</h3>
+      <div className="movie-detail-main">
+        <img src={poster_path} alt={title} className="poster" />
+        <div className="peared-items">
+          <img className="pear-point" src={pearshadow} alt="a pear" />
+          <span className="detail">Rating: {average_rating} / 10</span>
+        </div>
+        <div className="peared-items">
+          <img className="pear-point" src={pearshadow} alt="a pear" />
+          <span className="detail">Runtime: {runtime} minutes</span>
+        </div>
+        <div className="peared-items">
+          <img className="pear-point" src={pearshadow} alt="a pear" />
+          <span className="detail">Release Date: {release_date}</span>
+        </div>
+        <span className="movie-overview">{overview}</span>
       </div>
     </Link>
-  )
-} 
+  );
+};
 
 export default MovieDeets;
 
 MovieDeets.propTypes = {
-  selectedMovie : PropTypes.shape({
+  selectedMovie: PropTypes.shape({
     id: PropTypes.number.isRequired,
     poster_path: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
@@ -38,6 +57,6 @@ MovieDeets.propTypes = {
     tagline: PropTypes.string.isRequired,
     average_rating: PropTypes.number.isRequired,
     runtime: PropTypes.number.isRequired,
-    release_date: PropTypes.string.isRequired
-  }) 
-}
+    release_date: PropTypes.string.isRequired,
+  }),
+};
